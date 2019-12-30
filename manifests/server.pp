88759c350eee307af2e7836797d3dd4b435e4bec
Sam Marshall <sam@roker.com.au>
2019-12-30 15:13:03 +1000

    Remove weak DH primes

--- manifests/server.pp
+++ manifests/server.pp
@@ -284,4 +284,9 @@ class ssh_hardening::server (
     content => template("${module_name}/sshd_config.erb"),
   }
 
+  exec { 'Remove weak DH primes':
+    command => 'sed -i "/ \(1023\|1535\) /d" /etc/ssh/moduli',
+    path    => '/bin',
+  }
+
 }
